9dcddafed2cd397bf97cd67cccea3ca543a2ded3
Éverton Mengotti <dev@evert90.com>
2021-09-13 22:31:30 -0300

    refactor: update next + tailwind + tailwind/jit

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -19,13 +19,10 @@ export const Login: LayoutComponent = () => {
 
   useEffect(() => {
     // redirect to home if already logged in
-    console.log("aqui de novo", userService.getUserValue())
     if (userService.getUserValue()?.token) {
       router.push('/');
     }
-}, []);
-
-
+  }, []);
 
    // form validation rules
    const validationSchema = Yup.object().shape({
@@ -58,26 +55,26 @@ export const Login: LayoutComponent = () => {
 
   return (
     <>
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-4/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-              <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <h6 className="text-blueGray-500 text-sm font-bold">
+      <div className="container h-full px-4 mx-auto">
+        <div className="flex items-center content-center justify-center h-full">
+          <div className="w-full px-4 lg:w-4/12">
+            <div className="relative flex flex-col w-full min-w-0 mb-6 break-words border-0 rounded-lg shadow-lg bg-blueGray-200">
+              <div className="px-6 py-6 mb-0 rounded-t">
+                <div className="mb-3 text-center">
+                  <h6 className="text-sm font-bold text-blueGray-500">
                     Sign in with
                   </h6>
                 </div>
-                <div className="btn-wrapper text-center">
+                <div className="text-center btn-wrapper">
                   <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                    className="inline-flex items-center px-4 py-2 mb-1 mr-2 text-xs font-normal font-bold uppercase transition-all duration-150 ease-linear bg-white rounded shadow outline-none active:bg-blueGray-50 text-blueGray-700 focus:outline-none hover:shadow-md"
                     type="button"
                   >
                     <img alt="..." className="w-5 mr-1" src="/img/github.svg" />
                     Github
                   </button>
                   <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                    className="inline-flex items-center px-4 py-2 mb-1 mr-1 text-xs font-normal font-bold uppercase transition-all duration-150 ease-linear bg-white rounded shadow outline-none active:bg-blueGray-50 text-blueGray-700 focus:outline-none hover:shadow-md"
                     type="button"
                   >
                     <img alt="..." className="w-5 mr-1" src="/img/google.svg" />
@@ -86,14 +83,14 @@ export const Login: LayoutComponent = () => {
                 </div>
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
               </div>
-              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <div className="text-blueGray-400 text-center mb-3 font-bold">
+              <div className="flex-auto px-4 py-10 pt-0 lg:px-10">
+                <div className="mb-3 font-bold text-center text-blueGray-400">
                   <small>Or sign in with credentials</small>
                 </div>
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="relative w-full mb-3">
                     <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                       htmlFor="grid-password"
                     >
                       Email
@@ -108,7 +105,7 @@ export const Login: LayoutComponent = () => {
 
                   <div className="relative w-full mb-3">
                     <label
-                      className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                      className="block mb-2 text-xs font-bold uppercase text-blueGray-600"
                       htmlFor="grid-password"
                     >
                       Password
@@ -125,7 +122,7 @@ export const Login: LayoutComponent = () => {
                       <input
                         id="customCheckLogin"
                         type="checkbox"
-                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                        className="w-5 h-5 ml-1 transition-all duration-150 ease-linear border-0 rounded form-checkbox text-blueGray-700"
                       />
                       <span className="ml-2 text-sm font-semibold text-blueGray-600">
                         Remember me
@@ -133,20 +130,20 @@ export const Login: LayoutComponent = () => {
                     </label>
                   </div>
 
-                  <div className="text-center mt-6">
+                  <div className="mt-6 text-center">
                     <button
-                      className="disabled:opacity-50 bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                      className="w-full px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none disabled:opacity-50 bg-blueGray-800 active:bg-blueGray-600 hover:shadow-lg focus:outline-none"
                       type="submit"
                       disabled={formState.isSubmitting}
                     >
-                      {formState.isSubmitting && <i className="fas fa-circle-notch animate-spin text-white mx-auto text-1xl mr-1"></i>}
+                      {formState.isSubmitting && <i className="mx-auto mr-1 text-white fas fa-circle-notch animate-spin text-1xl"></i>}
                       Sign In
                     </button>
                   </div>
                 </form>
               </div>
             </div>
-            <div className="flex flex-wrap mt-6 relative">
+            <div className="relative flex flex-wrap mt-6">
               <div className="w-1/2">
                 <a
                   href="#pablo"
