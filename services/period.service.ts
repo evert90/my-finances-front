5327a606edb34b4f09430cb3cee4e01e486c7403
Éverton Mengotti <dev@evert90.com>
2021-09-26 18:08:20 -0300

    feat: add charts on demand

--- services/period.service.ts
+++ services/period.service.ts
@@ -1,17 +1,18 @@
 import { Period } from "../class/Period";
 import moment from "moment"
 import { PeriodTotal } from "../class/PeriodTotal";
+import { PeriodTagTotal } from "../class/PeriodTagTotal";
 
 export const periodService = {
     getPeriodMonths,
-    getPeriodTotalMonths
+    getPeriodTotalMonths,
+    getPeriodTagTotalMonths
 }
 
 function getPeriodMonths(meses: number): Array<Period> {
     let periodos: Array<Period> = [];
 
     for(let i = -1; i - 1 < meses - 2; i++) {
-        console.log("aqui", i)
         let dataInicial = moment()
             .startOf('month')
             .subtract(i, 'months')
@@ -32,7 +33,6 @@ function getPeriodTotalMonths(meses: number): Array<PeriodTotal> {
     let periodos: Array<PeriodTotal> = [];
 
     for(let i = -1; i - 1 < meses - 2; i++) {
-        console.log("aquit", i)
         let dataInicial = moment()
             .startOf('month')
             .subtract(i, 'months')
@@ -44,6 +44,26 @@ function getPeriodTotalMonths(meses: number): Array<PeriodTotal> {
             .format('YYYY-MM-DD');
 
         periodos.push(new PeriodTotal(dataInicial, dataFinal, []))
+    }
+
+    return periodos
+}
+
+function getPeriodTagTotalMonths(meses: number): Array<PeriodTagTotal> {
+    let periodos: Array<PeriodTagTotal> = [];
+
+    for(let i = -1; i - 1 < meses - 2; i++) {
+        let dataInicial = moment()
+            .startOf('month')
+            .subtract(i, 'months')
+            .format('YYYY-MM-DD');
+
+        let dataFinal = moment()
+            .endOf('month')
+            .subtract(i, 'months')
+            .format('YYYY-MM-DD');
+
+        periodos.push(new PeriodTagTotal(dataInicial, dataFinal, []))
     }
 
     return periodos
