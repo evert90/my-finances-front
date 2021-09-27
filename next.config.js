a66a821819b34b9d19d77dec0196d12489ac625a
Éverton Mengotti <fulanodetal@exemplo.br>
2021-09-26 22:06:53 -0300

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+    pwa: {
+      dest: 'public',
+      runtimeCaching,
+    },
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'https://192.168.0.24:21200/api' // development api
             : 'https://192.168.0.24:21200/api' // production api
     }
-
-}
+})
