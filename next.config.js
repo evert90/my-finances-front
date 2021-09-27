8da9ef04d00b6a194d65bef87dab0c85d9ac59ca
Éverton Mengotti <fulanodetal@exemplo.br>
2021-09-26 21:49:00 -0300

    chore: adjusts login

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
-            ? 'https://desktop-27v6i4N:21200/api' // development api
-            : 'https://desktop-27v6i4N:21200/api' // production api
+            ? 'https://192.168.0.24:21200/api' // development api
+            : 'https://192.168.0.24:21200/api' // production api
     }
 
 }
