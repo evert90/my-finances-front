9f0aeac39dc734d25bf669fffda6a5f84c67ebf7
Éverton Mengotti <fulanodetal@exemplo.br>
2021-09-29 01:24:32 -0300

    feat: slider mobile adjusts with swiper

--- components/Cards/CardFinancialRecord.tsx
+++ components/Cards/CardFinancialRecord.tsx
@@ -27,7 +27,7 @@ export const CardFinancialRecord: React.FC<CardFinancialRecordProps> = (props) =
     return (
         <>
             <div className="relative flex flex-col w-full min-w-0 break-words bg-white rounded shadow-lg cursor-pointer lg:mb-7 scale-005"
-                 onClick={() => setShowInfo(!showInfo)}
+
             >
                 <div className="px-2 py-3 mb-0 border-0 rounded-t">
                     <div className="flex flex-wrap items-center">
@@ -38,6 +38,7 @@ export const CardFinancialRecord: React.FC<CardFinancialRecordProps> = (props) =
                         </div>
                         <div className="relative flex-1 flex-grow w-full max-w-[25px] px-1 text-right">
                             <i className={`mt-1 mr-1 cursor-pointer text-base fas ${showInfo ? 'fa-chevron-circle-up' : 'fa-chevron-circle-down'} `}
+                               onClick={() => setShowInfo(!showInfo)}
                             ></i>
                         </div>
                     </div>
