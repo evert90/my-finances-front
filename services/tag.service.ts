89e0393ee085e8d7ffc32f4ebd261de67bc6bbb3
Éverton Mengotti <dev@evert90.com>
2021-10-01 22:53:19 -0300

    feat: apiUrl via local storage

--- services/tag.service.ts
+++ services/tag.service.ts
@@ -2,7 +2,7 @@ import getConfig from 'next/config';
 import { fetchWrapper } from '../helpers/fetch-wrapper';
 
 const { publicRuntimeConfig } = getConfig();
-const baseUrl = `${publicRuntimeConfig.apiUrl}/tags`;
+const baseUrl = process.browser && localStorage.getItem("apiUrl") ? `${localStorage.getItem("apiUrl")}/tags` : `${publicRuntimeConfig.apiUrl}/tags`;
 
 export const tagService = {
     getAll
