15b2cbcc0418015659df564b248efb23e5a3ebb8
Éverton Mengotti <dev@evert90.com>
2021-10-10 04:47:33 -0300

    feat: adjusts table and widths

--- components/Cards/CardTableOnDemand.tsx
+++ components/Cards/CardTableOnDemand.tsx
@@ -1,7 +1,11 @@
+import moment from "moment";
 import { CardOnDemand } from "../../class/CardOnDemand"
 import { CardOnDemandFilterBy } from "../../class/CardOnDemandFilterBy"
+import { FinancialRecordType } from "../../class/FinancialRecordType";
 import { PeriodTagTotal } from "../../class/PeriodTagTotal"
 import { PeriodTotal } from "../../class/PeriodTotal"
+import { currencyService } from "../../services/currency.service";
+import { periodService } from "../../services/period.service"
 
 type CardTableOnDemandProps = {
     cardOnDemand: CardOnDemand
@@ -10,59 +14,102 @@ type CardTableOnDemandProps = {
 export const CardTableOnDemand: React.FC<CardTableOnDemandProps> = (props) => {
     return (
         <>
-        <div className={"relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white"}>
-            <div className="px-4 py-3 mb-0 border-0 rounded-t">
-                <div className="flex flex-wrap items-center">
-                    <div className="relative flex-1 flex-grow w-full max-w-full px-1">
-                        <h3 className={"text-xl font-bold text-blueGray-700"}>
-                            Lançamentos
-                        </h3>
-                    </div>
-                </div>
-            </div>
+        <div
+            className={`relative flex flex-col min-w-0 break-words w-full shadow-lg rounded bg-white`}
+            style={{minHeight: props.cardOnDemand.height + "px"}}
+        >
             <div className="block w-full overflow-x-auto">
-                <table className="items-center table w-full bg-transparent border-collapse stripped">
+                <table className="items-center table w-full text-sm bg-transparent border-collapse stripped">
                     <thead>
                         <tr>
-                            <th className={"column-fit thead-column"}>
+                            <th className={"card-on-demand-thead-column"}>
                                 Período
                             </th>
                             {CardOnDemandFilterBy[props.cardOnDemand.filterBy] === CardOnDemandFilterBy.INCOME_EXPENSE && [
-                                <th key={0} className={"thead-column"}>
+                                <th key={0} className={"card-on-demand-thead-column "}>
                                     Receitas
                                 </th>,
-                                <th key={1} className={"thead-column"}>
+                                <th key={1} className={"card-on-demand-thead-column "}>
                                     Despesas
+                                </th>,
+                                <th key={2} className={"card-on-demand-thead-column "}>
+                                    Diferença
+                                </th>,
+                                <th key={3} className={"card-on-demand-thead-column "}>
+                                    Total
                                 </th>
                             ]}
-                            {CardOnDemandFilterBy[props.cardOnDemand.filterBy] === CardOnDemandFilterBy.TAGS &&
-                            props.cardOnDemand.tags.map((tag, index) =>
-                                <th key={index} className={"thead-column"}>
-                                    {tag.name}
+                            {CardOnDemandFilterBy[props.cardOnDemand.filterBy] === CardOnDemandFilterBy.TAGS && [
+                                props.cardOnDemand.tags.map((tag, index) =>
+                                    <th key={index} className={"card-on-demand-thead-column "}>
+                                        {tag.name}
+                                    </th>
+                                ),
+                                <th key={props.cardOnDemand.tags.length + 1} className={"card-on-demand-thead-column "}>
+                                    Diferença
+                                </th>,
+                                <th key={props.cardOnDemand.tags.length + 2} className={"card-on-demand-thead-column "}>
+                                    Total
                                 </th>
-                            )}
+                            ]}
                         </tr>
                     </thead>
                     <tbody>
-                    {(CardOnDemandFilterBy[props.cardOnDemand.filterBy] === CardOnDemandFilterBy.INCOME_EXPENSE && (props.cardOnDemand?.data as Array<PeriodTotal>).map(period =>
-                        <tr key={period.start} v-for="row in rows" className="odd:bg-blueGray-50 even:bg-white hover:bg-blueGray-100">
-                            <td className="flex items-center justify-center p-4 px-6 text-base align-middle border-t-0 border-l-0 border-r-0 whitespace-nowrap">
+                    {(CardOnDemandFilterBy[props.cardOnDemand.filterBy] === CardOnDemandFilterBy.INCOME_EXPENSE && (props.cardOnDemand?.data as Array<PeriodTotal>).map((period, index) => {
+                        const incomes: number = period?.totals?.find(it => FinancialRecordType[it.type] == FinancialRecordType.INCOME)?.total || 0
+                        const expenses: number = period?.totals?.find(it => FinancialRecordType[it.type] == FinancialRecordType.EXPENSE)?.total || 0
+                        const total = incomes - expenses
+                        let totalPreviousPeriod = undefined
+                        if(index + 1 <= props.cardOnDemand?.data.length) {
+                            const incomesPreviousMonth: number = (props.cardOnDemand?.data[index+1] as PeriodTotal)?.totals?.find(it => FinancialRecordType[it.type] == FinancialRecordType.INCOME)?.total || 0
+                            const expensesPreviousMonth: number = (props.cardOnDemand?.data[index+1] as PeriodTotal)?.totals?.find(it => FinancialRecordType[it.type] == FinancialRecordType.EXPENSE)?.total || 0
+                            totalPreviousPeriod = incomesPreviousMonth - expensesPreviousMonth
+                        }
 
+                        return <tr key={period.start} className="border-t-[1px]">
+                            <td className="capitalize card-on-demand-tbody-column">
+                                {moment(period.start, 'YYYY-MM-DD').locale("pt-BR").format(periodService.periodTypeToDateFormat(props.cardOnDemand.periodType))}
                             </td>
-                            <td className="p-4 px-6 text-base align-middle border-t-0 border-l-0 border-r-0 whitespace-nowrap">{record.name}</td>
-                            <td className="p-4 px-6 text-base align-middle border-t-0 border-l-0 border-r-0 whitespace-nowrap"><Moment date={record.date} format="DD/MM/YYYY" /></td>
-                            <td className="p-4 px-6 text-base align-middle border-t-0 border-l-0 border-r-0 whitespace-nowrap">{currencyService.format(record.value)}</td>
-                            <td className="p-4 px-6 text-base align-middle border-t-0 border-l-0 border-r-0 whitespace-nowrap">
-                            {record.tags.map(tag =>
-                                <span key={tag.id} className="text-xs px-2 py-0.5 rounded ml-1 font-bold bg-green-500 text-white">
-                                {tag.name}
-                                </span>
-                            )}
+                            <td className=" card-on-demand-tbody-column">
+                                {currencyService.format(incomes)}
                             </td>
-
+                            <td className=" card-on-demand-tbody-column">
+                                {currencyService.format(expenses)}
+                            </td>
+                            <td className=" card-on-demand-tbody-column">
+                                {total != 0 && totalPreviousPeriod ? currencyService.format(total - totalPreviousPeriod) : "-"}
+                            </td>
+                            <td className={`card-on-demand-tbody-column `}>
+                                {currencyService.format(total)}
+                            </td>
                         </tr>
-                    ))}
+                    }))}
+                    {(CardOnDemandFilterBy[props.cardOnDemand.filterBy] === CardOnDemandFilterBy.TAGS && (props.cardOnDemand?.data as Array<PeriodTagTotal>).map((period, index) => {
+                        let total: number = 0;
+                        let totalPreviousPeriod = undefined
+                        if(index + 1 <= props.cardOnDemand?.data.length) {
+                            totalPreviousPeriod = (props.cardOnDemand?.data[index+1] as PeriodTagTotal)?.totals?.map(it => it.total).reduce((a, b) => a + b, 0) || 0
+                        }
 
+                        return <tr key={period.start} className="border-t-[1px]">
+                            <td className="capitalize card-on-demand-tbody-column">
+                                {moment(period.start, 'YYYY-MM-DD').locale("pt-BR").format(periodService.periodTypeToDateFormat(props.cardOnDemand.periodType))}
+                            </td>
+                            {props.cardOnDemand.tags.map((tag, index) =>{
+                                const tagTotal = period?.totals?.find(it => it.tag.name == tag.name)?.total
+                                total+= tagTotal || 0
+                                return <td key={index} className={"card-on-demand-tbody-column "}>
+                                    {tagTotal ? currencyService.format(tagTotal) : "-"}
+                                </td>
+                                })}
+                            <td className="card-on-demand-tbody-column">
+                                {total != 0  && totalPreviousPeriod != 0 ? currencyService.format(total - totalPreviousPeriod) : "-"}
+                            </td>
+                            <td className={`card-on-demand-tbody-column `}>
+                                {currencyService.format(total)}
+                            </td>
+                        </tr>
+                    }))}
                     </tbody>
                 </table>
             </div>
