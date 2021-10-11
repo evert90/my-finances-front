f9f46a30a3432b9414a4ab2278e6cfbebca99cfa
Éverton Mengotti <dev@evert90.com>
2021-10-10 21:09:02 -0300

    feat: add order option in cards on demand

--- components/Dropdowns/CardOnDemandDropdown.tsx
+++ components/Dropdowns/CardOnDemandDropdown.tsx
@@ -5,7 +5,8 @@ import { CardOnDemand } from "../../class/CardOnDemand";
 type CardOnDemandDropdownProps = {
     cardOnDemand: CardOnDemand
     remove: Function,
-    edit: Function
+    edit: Function,
+    order: Function
 }
 
 export const CardOnDemandDropdown: React.FC<CardOnDemandDropdownProps> = (props) => {
@@ -32,35 +33,39 @@ export const CardOnDemandDropdown: React.FC<CardOnDemandDropdownProps> = (props)
         };
     return (
         <>
-            <a
-                className="text-blueGray-500"
-                href="#pablo"
-                ref={btnDropdownRef}
-                onClick={(e) => {
+            <a className="text-blueGray-500"
+               href="#pablo"
+               ref={btnDropdownRef}
+               onClick={(e) => {
                     e.preventDefault();
                     dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
-                }}
+               }}
             >
                 <i className="fas fa-ellipsis-v"></i>
             </a>
 
-            <div
-                ref={popoverDropdownRef}
-                className={
+            <div ref={popoverDropdownRef}
+                 className={
                     (dropdownPopoverShow ? "block " : "hidden ") +
                     "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-[10rem]"
                 }
             >
-                <a className={
-                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700 hover:bg-gray-100 cursor-pointer"
-                    }
-                    onClick={(e) => {closeDropdownPopover(); props.edit(props.cardOnDemand)}}
+                <a className={"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700 hover:bg-gray-100 cursor-pointer"}
+                   onClick={(e) => {closeDropdownPopover(); props.order(props.cardOnDemand, "UP")}}
+                >
+                    <i className="mr-2 fas fa-arrow-circle-up"></i> Ordenar
+                </a>
+                <a className={"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700 hover:bg-gray-100 cursor-pointer"}
+                   onClick={(e) => {closeDropdownPopover(); props.order(props.cardOnDemand, "DOWN")}}
+                >
+                    <i className="mr-2 fas fa-arrow-circle-down"></i> Ordenar
+                </a>
+                <a className={"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700 hover:bg-gray-100 cursor-pointer"}
+                   onClick={(e) => {closeDropdownPopover(); props.edit(props.cardOnDemand)}}
                 >
                     <i className="mr-2 fas fa-edit"></i> Editar
                 </a>
-                <a className={
-                    "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700 hover:bg-gray-100 cursor-pointer"
-                    }
+                <a className={"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700 hover:bg-gray-100 cursor-pointer"}
                     onClick={(e) => {closeDropdownPopover(); props.remove(props.cardOnDemand)}}
                 >
                     <i className="mr-2.5 fas fa-trash"></i> Remover
