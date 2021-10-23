11b93b62cc357d2f9e219f50589ca74cbbebf76d
Éverton Mengotti <dev@evert90.com>
2021-10-23 19:38:15 -0300

    feat: show percentage for all in pie/donut chart + conditional load on GA + anonymize ip by default
    Anonymize ip is enabled by default on GA4 https://support.google.com/analytics/answer/2763052?hl=pt-BR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -124,25 +124,26 @@ function MyApp({ Component, pageProps }) {
       <Providers>
         <Layout>
         {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-                anonymize_ip: true
-              });
-            `,
-          }}
-        />
+        {gtag.GA_TRACKING_ID && [
+          <Script
+            key="scriptGtag"
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />,
+          <Script
+            key="initGtag"
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gtag.GA_TRACKING_ID}', {'page_path': window.location.pathname});
+              `,
+            }}
+          />
+        ]}
           {authorized &&
             <Component {...pageProps} />
           }
