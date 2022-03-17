fe7c8f5b76e2c4386c10aaa8f54bd2d3a4dd037c
Éverton Mengotti <fulanodetal@exemplo.br>
2022-03-17 01:31:58 -0300

    fix: theme light color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="theme-color" media="(prefers-color-scheme: light)" content="white"></meta>
+          <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fffbfb"></meta>
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black"></meta>
           <link rel="shortcut icon" href="/img/brand/favicon.ico" />
           <link
