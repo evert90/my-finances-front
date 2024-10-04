9ead13ef7e593402569e27c8e46a65cd18d756c3
Éverton <dev@evert90.com>
2024-10-04 20:39:48 -0300

    feat: index redirect to dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 /* eslint-disable react/jsx-no-target-blank */
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import { IndexNavbar } from "../components/Navbars/IndexNavbar";
 import { Footer } from "../components/Footers/Footer";
+import { useRouter } from "next/router";
 
 export const Index = () => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push("/admin/dashboard");
+    })
+
     return (
         <>
             <IndexNavbar />
